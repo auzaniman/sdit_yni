70de1208aa68f1618b68a9fc98f0b55712bbf6a6
Auzan <auzan.ip@gmail.com>
2022-02-11 21:52:38 +0700

    first commit

---
+++ resources/views/user_view/includes/header.blade.php
@@ -0,0 +1,14 @@
+<header class="header-2">
+  <div class="page-header min-vh-100 relative" style="background-image: url('frontend/assets/img/SMAIT/1.jpeg')">
+    <span class="mask bg-gradient-dark opacity-9"></span>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-7 text-center mx-auto">
+          <h1 class="text-white pt-3 mt-n5">Selamat Datang</h1>
+          <h3 class="text-white">Web Resmi SMAIT Nurul 'Ilmi Tenggarong</h3>
+          <p class="lead text-white mt-3">"Menjadi lembaga pendidikan unggul dan profesional dalam mencetak SDM Islami yang berkarakter, kompetitif dan berwawasan global."</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
